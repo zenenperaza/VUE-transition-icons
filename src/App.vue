e5a7a733df6796dc526e863a961b6d1379a1c55f
zenenperaza <peraza@outlook.com>
2023-06-21 17:07:59 -0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
 <div> 
+  <button v-on:click="addNotes">Add Note</button>
+  <input type="text" v-model="note">
+  <TransitionGroup name="list" tag="ul">
+    <li v-for="note in notes" :key="note"> {{ note }} </li>
+  </TransitionGroup>
+
+
 </div>
 </template>
 
 <script lang="ts" setup>
-   
+import { ref } from "vue"
+
+const note = ref('')
+const notes = ref([])
+
+const addNotes = () => {
+  notes.value.push(note.value)
+  note.value = ''
+}
+
+
 </script>
 
 <style scoped>
-
+.list-enter-active, .list-leave-active{
+transition: all 0.5s ease;
+}
+.list-enter-from, .list-leave-to{
+opacity: 0;
+transform: translateX(30px);
+}
 </style>
